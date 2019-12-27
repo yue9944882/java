72325e20d577798af1eefa45d4f894166053fa65
yue9944882 <291271447@qq.com>
2019-12-27 16:19:11 +0800

    adding field selector dummy class

---
+++ util/src/main/java/io/kubernetes/client/util/labels/FieldSelector.java
@@ -0,0 +1,7 @@
+package io.kubernetes.client.util.labels;
+
+
+// Field Selector is bascally the same with the {@link io.kubernetes.client.util.label.LabelSelector}
+// but with a different name for clarification.
+public class FieldSelector extends LabelSelector {
+}
